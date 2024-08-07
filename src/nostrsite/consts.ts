60ccf75d99e9d8cb03c4351e8591455645541906
artur <brugeman.artur@gmail.com>
2024-08-07 16:00:41 +0200

    Update to search 1.0.11 and content-cta 1.0.2

--- src/nostrsite/consts.ts
+++ src/nostrsite/consts.ts
@@ -31,10 +31,10 @@ export const JS_VENOBOX = "https://cdn.jsdelivr.net/npm/venobox@2.1.8/dist/venob
 export const CSS_VENOBOX = "https://cdn.jsdelivr.net/npm/venobox@2.1.8/dist/venobox.min.css";
 export const JS_ZAPTHREADS = "https://cdn.npubpro.com/zapthreads.iife.0.5.5.js";
 export const JS_NOSTR_LOGIN = "https://unpkg.com/nostr-login@1.5.2/dist/unpkg.js";
-export const JS_SEARCH = "https://unpkg.com/nostr-site-search@1.0.10/dist/index.js";
+export const JS_SEARCH = "https://unpkg.com/nostr-site-search@1.0.11/dist/index.js";
 export const JS_ZAP = "https://cdn.npubpro.com/nostr-zap.0.22.0.js";
 export const JS_EMBEDS = "https://cdn.npubpro.com/embeds.iife.1.0.3.js";
-export const JS_CONTENT_CTA = "https://cdn.npubpro.com/content-cta.iife.1.0.1.js";
+export const JS_CONTENT_CTA = "https://cdn.npubpro.com/content-cta.iife.1.0.2.js";
 
 export const BLOSSOM_FALLBACKS = [
   "https://blossom.npubpro.com",
